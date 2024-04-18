031c320746eebdca89359ee50712770c82f2902a
tatianayr <99680574+tatianayr@users.noreply.github.com>
2024-04-18 10:44:16 +0100

    update

--- app/src/main/java/com/example/smartshelterapp/LoginActivity.java
+++ app/src/main/java/com/example/smartshelterapp/LoginActivity.java
@@ -28,57 +28,44 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Inicializar Firebase Database
         database = FirebaseDatabase.getInstance();
-        // Referência aos usuários no banco de dados
         usersRef = database.getReference("Users");
 
         binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Obter o nome de usuário e a senha inseridos pelo usuário
                 userName = binding.userName.getText().toString();
                 password = binding.password.getText().toString();
 
-                // Verificar se os campos estão preenchidos
                 if (TextUtils.isEmpty(userName) || TextUtils.isEmpty(password)) {
                     Toast.makeText(LoginActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
                     return;
                 }
-
-                // Autenticar o usuário
                 authenticateUser(userName, password);
             }
         });
     }
 
     private void authenticateUser(final String userName, final String password) {
-        // Adicionar um listener para recuperar os dados do usuário
         usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                // Verificar se o usuário existe e se a senha está correta
                 if (dataSnapshot.child(userName).exists()) {
                     Users user = dataSnapshot.child(userName).getValue(Users.class);
                     if (user != null && user.getPassword().equals(password)) {
-                        // Login bem-sucedido
                         Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(LoginActivity.this, MapsActivity.class));
 
-                        // Faça o que você precisa aqui, como navegar para outra tela
                     } else {
-                        // Senha incorreta
                         Toast.makeText(LoginActivity.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
                     }
                 } else {
-                    // Usuário não encontrado
                     Toast.makeText(LoginActivity.this, "User not found", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Tratamento de erro
                 Toast.makeText(LoginActivity.this, "Error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
--- app/src/main/java/com/example/smartshelterapp/MapsActivity.java
+++ app/src/main/java/com/example/smartshelterapp/MapsActivity.java
@@ -23,8 +23,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -43,7 +41,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
